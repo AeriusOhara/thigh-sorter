b8c1497a19111f06d2be0150173f8ed469544add
aeriussu <it.salih.ozkan@gmail.com>
2022-04-10 12:18:06 +0200

    Added renaming (f.ex.: to rename twitter's jfif extension.) Also changed the way actions are handled so they can be stacked.

--- sorter.py
+++ sorter.py
@@ -18,11 +18,25 @@ def sort():
             # Iterate over the items in the category
             for fileName in os.listdir(downloadsFolder):
                 # If the file extension of the file matches
-                if fileName.endswith(f'.{files["ext"]}'):
-                    # Check if the folder we're about to use exist
-                    folderExists(f'{sortingFolder}{files["arg"]}')
-                    # Move the file
-                    os.replace(f'{downloadsFolder}{fileName}', f'{sortingFolder}{files["arg"]}{fileName}')
+                if fileName.lower().endswith(f'.{files["ext"]}'):
+                    # Check what action we'll be taking
+                    for actions in files["act"]:
+                        if actions[0] == "ren_ext":
+                            # Split filename and extension
+                            fileNameOnly, fileExtension = os.path.splitext(fileName)
+
+                            # Rename the extension
+                            oldFileName = f'{fileName}'
+                            fileName = f'{fileNameOnly}.{actions[1]}'
+
+                            os.rename(f'{downloadsFolder}{oldFileName}', f'{downloadsFolder}{fileName}')
+                        elif actions[0] == "mov":
+                            # Check if the folder we're about to use exist
+                            folderExists(f'{sortingFolder}{actions[1]}')
+
+                            # Move the file
+                            os.replace(f'{downloadsFolder}{fileName}', f'{sortingFolder}{actions[1]}{fileName}')
+
 
 def folderExists(path):
     # Check if the folder exists
