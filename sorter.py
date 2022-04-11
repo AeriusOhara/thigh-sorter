e2aa66837c1cb4cdb3375420c0b8d24b2bb2fb4b
aeriussu <it.salih.ozkan@gmail.com>
2022-04-11 16:17:09 +0200

    Began using timestamp to avoid overwiting common filenames, and used os.chdir to change the working directory so there'd be less passing around of directory paths.

--- sorter.py
+++ sorter.py
@@ -1,48 +1,48 @@
 import os
 import json
+from datetime import datetime
 
 downloadsFolder = "E:/Downloads/"
 sortingFolder = "E:/Downloads/SORTED/"
 
 def setup():
-    folderExists(downloadsFolder)
-    folderExists(sortingFolder)
+	folderExists(downloadsFolder)
+	folderExists(sortingFolder)
 
 def sort():
-    with open("sortingRules.json") as jsonList:
-        data = json.load(jsonList)
+	with open("sortingRules.json") as jsonList:
+		data = json.load(jsonList)
 
-    # Process each category one by one
-    for category in data.values():
-        for files in category:
-            # Iterate over the items in the category
-            for fileName in os.listdir(downloadsFolder):
-                # If the file extension of the file matches
-                if fileName.lower().endswith(f'.{files["ext"]}'):
-                    # Check what action we'll be taking
-                    for actions in files["act"]:
-                        if actions[0] == "ren_ext":
-                            # Split filename and extension
-                            fileNameOnly, fileExtension = os.path.splitext(fileName)
+	# Change working directory to where we're processing files
+	os.chdir(downloadsFolder)
 
-                            # Rename the extension
-                            oldFileName = f'{fileName}'
-                            fileName = f'{fileNameOnly}.{actions[1]}'
+	for category in data.values():
+		for files in category:
+			for fileName in os.listdir(downloadsFolder):
+				if fileName.lower().endswith(f'.{files["ext"]}'):
+					# Process actions
+					for action in files["act"]:
+						# Use timestamp in filename to avoid replacing common
+						# filenames. Example: Discord's 'unknown.png/jpg'
+						timestamp = int(datetime.timestamp(datetime.now()))
+						fileNameOnly, fileExtension = os.path.splitext(fileName)
 
-                            os.rename(f'{downloadsFolder}{oldFileName}', f'{downloadsFolder}{fileName}')
-                        elif actions[0] == "mov":
-                            # Check if the folder we're about to use exist
-                            folderExists(f'{sortingFolder}{actions[1]}')
+						if action[0] == "ren_ext":
+							prevFileName = f'{fileName}'
+							fileExtension = action[1]
+							fileName = f'{fileNameOnly}.{fileExtension}'
 
-                            # Move the file
-                            os.replace(f'{downloadsFolder}{fileName}', f'{sortingFolder}{actions[1]}{fileName}')
+							os.rename(prevFileName, fileName)
+						elif action[0] == "mov":
+							sortedFileName = f'{fileNameOnly}_{timestamp}{fileExtension}'
 
+							folderExists(f'{sortingFolder}{action[1]}')
+							os.replace(fileName, f'{sortingFolder}{action[1]}{sortedFileName}')
 
 def folderExists(path):
-    # Check if the folder exists
-    if not (os.path.exists(path)):
-        # If it doesn't, create it
-        os.makedirs(path)
+	# Create the folder if it doesn't exist
+	if not (os.path.exists(path)):
+		os.makedirs(path)
 
 setup()
 sort()
